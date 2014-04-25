a605b1b2662f1f59f2b3175497852d73c91c7988
Lewis Nixon <lewcfn@gmail.com>
2014-04-25 16:50:26 +0100

    adding tests for the Userservice and menu controller - last commit left these files out

---
+++ test/spec/controllers/menuctrl.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('Controller: MenuCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('myJamApp'));
+
+  var scope, MenuCtrl;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    
+    scope = $rootScope.$new();
+    MenuCtrl = $controller('MenuCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should assign the navigation array to the scope', function() {
+    expect(scope.navigation).toBeDefined();
+  });
+
+  it('should assign a goTo menu function to the scope', function() {
+    expect(typeof scope.goTo).toEqual('function');
+  });  
+});
--- test/spec/services/userService.js
+++ test/spec/services/userService.js
@@ -3,18 +3,17 @@
 describe('Service: UserService', function () {
   
   // instantiate service
-  var UserService,
-    httpBackEnd;
+  var httpBackEnd,
+    UserService;
 
   // load the service's module
   beforeEach(module('myJamApp'));
 
-  beforeEach(inject(function ($httpBackend, _UserService_) {
-    UserService = _UserService_;
-    httpBackEnd = $httpBackend;
-  }));
-
   describe('Service Initialisation', function() {
+    
+    beforeEach(inject(function (_UserService_) {
+      UserService = _UserService_;
+    }));    
     // basic menu init test
     it('should do something', function () {
       expect(!!UserService).toBe(true);
@@ -22,9 +21,22 @@ describe('Service: UserService', function () {
   });
 
   describe('API Response: Person Object', function() {
-    // basic menu init test
+    beforeEach(inject(function ($httpBackend, _UserService_) {
+      httpBackEnd = $httpBackend;
+      UserService = _UserService_;
+
+      httpBackEnd.expectGET('http://api.thisismyjam.com/1/boxdeluxe.json').respond({
+        "person": {
+            "apiUrl": "http://www.thisismyjam.com/1/boxdeluxe.json"
+        }
+      });
+
+    }));    
     it('the person object should contain a jam avatar', function () {
-      // expect(!!Menu).toBe(true);
+      // expect the returned object to be undefined until we flush the backend
+      expect(UserService.userData()).toBeUndefined();
+      httpBackEnd.flush();
+      expect(UserService.userData()).toBeDefined();
     });
   });
 
