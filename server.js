75e847e458a7a5ed455f99adc9430f669e6961db
Lewis Nixon <lewcfn@gmail.com>
2014-05-21 22:55:29 +0100

    adding the route and thing models so the rest api os sort of working now

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var db = mongoose.connect(config.mongo.uri, config.mongo.options);
 var modelsPath = path.join(__dirname, 'lib/models');
 fs.readdirSync(modelsPath).forEach(function (file) {
   if (/(.*)\.(js$|coffee$)/.test(file)) {
+    console.log('file is: ', modelsPath + '/' + file);
     require(modelsPath + '/' + file);
   }
 });
@@ -32,7 +33,7 @@ require('./lib/config/dummydata');
 // Setup Express
 var app = express();
 require('./lib/config/express')(app);
-// require('./lib/routes')(app);
+require('./lib/routes')(app);
 
 // Start server
 app.listen(config.port, config.ip, function () {
